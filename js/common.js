2c184065b77db12baa61d92d5414a09513fe2884
sanykos <sanykos446@gmail.com>
2020-06-13 16:40:02 +0300

    initial commit

---
+++ js/common.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    //console.log('test');
+    const btn = document.querySelector('.btn');
+    const body = document.querySelector('body');
+    const title = document.querySelector('h1');
+    //let backgroundColor = '#ffffff';
+
+    
+});
